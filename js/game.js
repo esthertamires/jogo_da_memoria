8b0450cfde7bfaadc324fbf2d40f5b0fe8f2d79f
esthertamires <esther_tamires@hotmail.com>
2024-02-28 15:35:44 -0300

    adicionando funcionalidade

--- js/game.js
+++ js/game.js
@@ -1,15 +1,33 @@
 const grid = document.querySelector('.grid'); // para selecionar uma classe é necessario adicionar um ponto "."
 
+const personagens = [ 
+    'Reyna',
+    'Astra',
+    'Brimstone',
+    'Harbor',
+    'Iso',
+    'Jett',
+    'Neon',
+    'Omen',
+    'Raze',
+    'Viper'
+]
+
 const createElement = (tag, className) =>{
     const element = document.createElement(tag); // criando um elemento 
     element.className = className; // a class que passou dentro do elemento
     return element; //retornando o elemento
 }
-const createCard = () => {
+
+//cria a carta
+
+const createCard = (personagens) => {
     
     const card = createElement('div', 'card'); // createElement é utilizado para criar um elemento›
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('../imagens/${personagens}.png')`
 
 
     card.appendChild(front); //appendChild  adiciona um filho na div card
@@ -17,3 +35,15 @@ const createCard = () => {
 
     return card;
 }
+
+const loadGame = () => {
+
+    //vai criar as cards
+    personagens.forEach((personagens) => {
+        const card = createCard(personagens);
+        grid.appendChild(card);
+    });
+
+}
+
+loadGame();
