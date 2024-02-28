0a375e3d44a50df6f629ee7707075b3fce633a22
esthertamires <esther_tamires@hotmail.com>
2024-02-28 14:39:33 -0300

    atualizaçao de imagens e adicionando elementos

--- js/game.js
+++ js/game.js
@@ -1,21 +1,19 @@
 const grid = document.querySelector('.grid'); // para selecionar uma classe é necessario adicionar um ponto "."
 
-
+const createElement = (tag, className) =>{
+    const element = document.createElement(tag); // criando um elemento 
+    element.className = className; // a class que passou dentro do elemento
+    return element; //retornando o elemento
+}
 const createCard = () => {
     
-    const card = document.createElement('div'); // createElement é utilizado para criar um elemento›
-    const front = document.createElement('div');
-    const back = document.createElement('div');
+    const card = createElement('div', 'card'); // createElement é utilizado para criar um elemento›
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
 
-    card.className = 'card';
-    front.className = 'face front';
-    back.className = 'face back';
 
     card.appendChild(front); //appendChild  adiciona um filho na div card
     card.appendChild(back);
 
-    grid.append(card);
+    return card;
 }
-
-createCard();
-console.log("teste")
